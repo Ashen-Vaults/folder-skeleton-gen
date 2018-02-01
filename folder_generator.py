266e16cb83e154c9888e1f3acef686a11c4b69f3
Ashen-Vaults <alexhollander522@gmail.com>
2018-02-01 16:55:59 -0500

    Refactored Code into functions
    Made the code more readable with comments and separate functions.

--- folder_generator.py
+++ folder_generator.py
@@ -6,23 +6,46 @@ import settings
 
 
 def get_json(path):
+    '''Returns the json data as data frame'''
     with open(path) as fd:
         return [x for x in pd.read_json(path)[settings.root]]
 
 
-def create_folder(directory):
+def create_folder(path):
+    '''Attempts to create a folder at specified path
+    Returns success. TODO: store fails, and allow for
+    re-attempt
+    '''
     try:
-        if not os.path.exists(directory):
-            os.makedirs(''+directory)
+        if not os.path.exists(path):
+            os.makedirs(path)
+            return True
     except OSError:
-        print('Error: Creating directory. ' + directory)
-    return directory
+        print('Error: Creating directory. ' + path)
+        return False
 
 
-def generate(json):
+def parse(json):
+    '''Returns the folders we want from json'''
     structure = get_json(json)
-    items = [x for folder in structure for x in folder]
+    return [x for folder in structure for x in folder]
+
+
+def generate_parents(items):
+    '''Creates top level folders'''
     [create_folder(items[i]['name']) for i, folder in enumerate(items) if items[i]['type'] == 'folder']
+
+
+def generate_children(items):
+    ''' Creates children folders, if any exist '''
     [create_folder(str(i['name']) + '/' + str(c['name'])) for i in items if 'children' in i for c in i['children']]
+
+
+def generate(file):
+    '''Takes in path to json and creates folders'''
+    items = parse(file)
+    generate_parents(items)
+    generate_children(items)
+
 
 generate('data/unity.json')
